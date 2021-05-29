b5b872dfbfebc0f124e2cb45f59962d1f4c3dba8
ruben <s236819@studenti.polito.it>
2021-05-29 18:18:04 +0200

    implemented class to save in a file the results

--- utils.py
+++ utils.py
@@ -10,4 +10,24 @@ def map_label(labels, actual_classes):
     labels = labels.cpu().numpy()
     for i in range(len(labels)):
         labels[i] = map[labels[i]]
-    return torch.LongTensor(labels).cuda() # remove cuda if GPU busy
+    return torch.LongTensor(labels).cuda()  # remove cuda if GPU busy
+
+
+class FileWriter():
+    '''
+    Class able to save in a file the result of the training/ testing 
+    process
+    '''
+
+    def __init__(self, filename):
+        
+        self.datafile = open(filename, 'w')
+
+        print('seed\tsplit_0\tsplit_1\tsplit_2\tsplit_3\tsplit_4\tsplit_5\tsplit_6\tsplit_7\tsplit_8\tsplit_9',
+         file=filename)
+
+    def register_seed(self, accuracy_list):
+        print(*accuracy_list, sep='\t', file=self.datafile)
+
+    def close_file(self):
+        self.datafile.close()
