6f6077a5a78251a2aaf573d95efe2089f937a69b
ruben <s236819@studenti.polito.it>
2021-06-15 12:06:43 +0200

    exemplars management v2

--- icarl.py
+++ icarl.py
@@ -1,6 +1,6 @@
-# class means for the actual classes
 exemplars_means = {}
 features = {}
+exemplars = {}
 
 for act_class in trainset.actual_classes:
 
@@ -14,18 +14,18 @@ for act_class in trainset.actual_classes:
     features[act_class] = img.detach().cpu().numpy()
     mean = torch.mean(img, 0) # column
     exemplars_means[act_class] = mean.detach().cpu().numpy()
+  
+  exemplar = []
+  cl_mean = np.zeros((1, 64))
 
+  for i in range(3):
+    x = exemplars_means[act_class] - (cl_mean + features [act_class]) / i+1
+    #print(x.shape)
+    x = np.linalg.norm(x, axis=1)
+    #print(x.shape)
+    index = np.argmin(x)
+    #print(index)
+    cl_mean += features[act_class][index]
+    exemplar.append(loader.dataset[index])
 
-# build exemplars set for a single class
-exemplar = []
-cl_mean = np.zeros((1, 64))
-
-for i in range(3): # must be m
-  x = exemplars_means[4] - (cl_mean + features [4]) / i+1
-  print(x.shape)
-  x = np.linalg.norm(x, axis=1)
-  print(x.shape)
-  index = np.argmin(x)
-  print(index)
-  cl_mean += features[4][index]
-  exemplar.append(loader.dataset[index])
+  exemplars[act_class] = exemplar
