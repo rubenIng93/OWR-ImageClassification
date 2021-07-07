fa694515395b7d119b05924575c2f18a28e7ff29
ruben <s236819@studenti.polito.it>
2021-07-07 17:14:17 +0200

    prova classifier

---
+++ prova.py
@@ -0,0 +1,37 @@
+import math
+
+import torch
+from torch.nn.parameter import Parameter
+from torch.nn import functional as F
+from torch.nn import Module
+
+class CosineLinear(Module):
+    def __init__(self, in_features, out_features, sigma=True):
+        super(CosineLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.Tensor(out_features, in_features))
+        if sigma:
+            self.sigma = Parameter(torch.Tensor(1))
+        else:
+            self.register_parameter('sigma', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.sigma is not None:
+            self.sigma.data.fill_(1) #for initializaiton of sigma
+
+    def forward(self, input):
+        #w_norm = self.weight.data.norm(dim=1, keepdim=True)
+        #w_norm = w_norm.expand_as(self.weight).add_(self.epsilon)
+        #x_norm = input.data.norm(dim=1, keepdim=True)
+        #x_norm = x_norm.expand_as(input).add_(self.epsilon)
+        #w = self.weight.div(w_norm)
+        #x = input.div(x_norm)
+        out = F.linear(F.normalize(input, p=2,dim=1), \
+                F.normalize(self.weight, p=2, dim=1))
+        if self.sigma is not None:
+            out = self.sigma * out
+        return out
